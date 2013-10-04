b594b1f94f0924f0393696cd5db9c7f28ff1fc8d
Simone D'Amico <simone.damico@yithemes.com>
2013-10-04 03:00:02 +0200

    AutoFiller methods

--- assets/js/autoFiller.js
+++ assets/js/autoFiller.js
@@ -7,19 +7,56 @@ AutoFiller.prototype = (function(){
         [].forEach.call(
             fields,
             function(input){
-                _fillField({
-                    field : input,
-                    tagName : input.tagName.toLowerCase(),
-                    name : input.name,
-                    id : input.id,
-                    type : input.type
-                });
+                _fillField(input);
             }
         );
     },
 
+
+    /**
+     * Fill each field according to its type and id/name
+     *
+     * Types:
+     *
+     * - password
+     * - date
+     * - month
+     * - email
+     * - checkbox
+     * - radio
+     * - textarea
+     * - select
+     * - number
+     * - tel
+     * - url
+     * - range
+     * - text => Check the id and/or the name of the field
+     *           in order to fill with appropriate value
+     */
     _fillField = function( field ) {
-        console.log(field);
+        var type = field.type.toLocaleLowerCase();
+        if( type != 'submit' && !field.disabled && !field.readOnly ) {
+            if( type == 'checkbox' || type == 'radio' ) {
+                //TODO: add checked attribute
+                console.log(field);
+            } else {
+                field.value = _getValueByType( type, field.id, field.name );
+            }
+        }
+    },
+
+    /**
+     * Return the right field value according its type
+     */
+    _getValueByType = function( type, id, name ) {
+        var value = null;
+
+        //TODO serve the right value according to type
+        if( type == '' ) {
+
+        }
+
+        return value;
     }
 
     return {
